b1fe4f4172ea7fa43e817f77f3bb91c9904733af
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2021-09-10 14:17:57 +0530

    Close fin in finally block

--- Chapter 13/ShowFile.java
+++ Chapter 13/ShowFile.java
@@ -6,28 +6,23 @@ public class ShowFile{
 			System.out.println("Usage: java ShowFile <filename>");
 			return;
 		}
-		FileInputStream fin;
+		FileInputStream fin = null;
 		try{
 			fin = new FileInputStream(args[0]);
-		}catch (FileNotFoundException e){
-			System.out.println("File Not Found: " + args[0]);
-			return;
-		}
-
-		int i;
-		try{
+			int i;
+		
 			do{
 				i = fin.read();
 				if(i != -1) System.out.println((char)i);	
 			}while(i != -1);
 		} catch (IOException e){
 			System.out.println("Can't read file");
-		}
-
-		try{
-			fin.close();
-		}catch(IOException e){
-			System.out.println("Error closing the file");
+		} finally {
+			try{
+				if(null != fin) fin.close();
+			} catch(IOException e){
+				System.out.println("Error while closing file" + e);
+			}
 		}
 	}
 }
