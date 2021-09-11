331db1b91dc479f1b767ffd830b5935f2cfbf81a
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2021-09-11 22:25:04 +0530

    Invoking overloaded constructor - without demo

---
+++ Chapter 13/MyClass.java
@@ -0,0 +1,19 @@
+class MyClass{
+	int a, b;
+	MyClass(int i, int j){
+		a = i;
+		a = j;
+	}
+	MyClass(int i){
+		a = i;
+		b = i;
+	}
+	MyClass(){
+		a = 0;
+		b = 0;
+	}
+
+	public static void main(String[] args){
+
+	}
+}
