6b328097fc5cd95d3d12ac20a8f96aadd23af28c
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2021-09-11 21:14:13 +0530

    Static Import demo

--- Chapter 13/Hypot.java
+++ Chapter 13/Hypot.java
@@ -1,10 +1,12 @@
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 class Hypot{
 	public static void main(String[] args){
 		double sideA, sideB;
 		double hypot;
 		sideA = 2.0;
 		sideB = 4.0;
-		hypot = Math.sqrt(Math.pow(sideA, 2.0) + Math.pow(sideB, 2.0));
+		hypot = sqrt(pow(sideA, 2.0) + pow(sideB, 2.0));
 		System.out.println("Given sides of lenth " + sideA + " and " + sideB + " the hypoteneous is " + hypot);
 	}
 }
