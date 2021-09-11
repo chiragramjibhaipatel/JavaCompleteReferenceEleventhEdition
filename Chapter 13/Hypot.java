490e66880229cfc5ebfa39e8f4d870dcd7dd59af
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2021-09-11 21:16:06 +0530

    import all static methods. Caution: use this wisely

--- Chapter 13/Hypot.java
+++ Chapter 13/Hypot.java
@@ -1,5 +1,5 @@
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
+import static java.lang.Math.*;
+
 class Hypot{
 	public static void main(String[] args){
 		double sideA, sideB;
