8717ccd194a3ba2e939e3ed38fa10b2b0aefdc3d
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2021-09-15 12:49:41 +0530

    Generics with wildcard

--- Chapter 14/BoundsDemo.java
+++ Chapter 14/BoundsDemo.java
@@ -2,13 +2,19 @@ class BoundsDemo{
 	
 	public static void main(String[] args){
 		
-		Double[] dNumbers = {1.1, 2.2 };
+		Double[] dNumbers = {1.0, 3.0, 4.0 };
 		Stats<Double> stats = new Stats<>(dNumbers);
 		System.out.println("Average of dNumbers is " + stats.average());
 
 		Integer[] iNumbers = {1, 3, 4};
 		Stats<Integer> iStats = new Stats<>(iNumbers);
 		System.out.println("Average of iNumbers is: " + iStats.average());
+	
+		if(stats.sameAverage(iStats)){
+			System.out.println("Average of iNumbers and dNumbers are same");
+		} else {
+			System.out.println("Average of iNumbers and dNumbers are not same");
+		}
 	}
 }
 
@@ -18,11 +24,20 @@ class Stats<T extends Number>{
 	public Stats(T[] o){
 		numbers = o;
 	}
+
 	double average(){
 		double sum = 0.0;
 		for(int i=0; i<numbers.length; i++){
 			sum += numbers[i].doubleValue();
 		}
 		return sum / numbers.length;
+	}
+	
+	boolean sameAverage(Stats<?> that){
+		if(average() == that.average()){
+			return true;
+		}
+		return false;
+
 	}
 }
