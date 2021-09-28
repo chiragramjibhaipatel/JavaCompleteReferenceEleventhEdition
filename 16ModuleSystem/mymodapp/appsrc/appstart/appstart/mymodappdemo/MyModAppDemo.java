677e76f7956657a328557c2d651be7d288946e0f
Chirag Ramjibhai Patel <chiragramjibhaipatel@gmail.com>
2021-09-28 15:32:16 +0530

    Module Demo

---
+++ 16ModuleSystem/mymodapp/appsrc/appstart/appstart/mymodappdemo/MyModAppDemo.java
@@ -0,0 +1,11 @@
+package appstart.mymodappdemo;
+
+import appfuncs.simplefuncs.SimpleMathFuncs;
+
+public class MyModAppDemo{
+	public static void main(String [] args){
+		if(SimpleMathFuncs.isFactor(2, 10)){
+			System.out.println("2 is factor of 10");
+		}
+	}
+}
